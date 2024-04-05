b39e1182266345d74df6dd25209e54b2301af8b8
15josh08 <joshrajendrapatel@gmail.com>
2024-04-05 12:46:19 -0400

    Changees for the front

--- streamlit1.py
+++ streamlit1.py
@@ -1,15 +1,16 @@
 import streamlit as st
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input, decode_predictions
+from keras.preprocessing import image
+from keras.applications.mobilenet_v2 import preprocess_input, decode_predictions
 import numpy as np
 import tensorflow
 
+# Function to clear uploaded image
+def clear_image():
+    st.uploaded_image = None
+
 # Load the model
 def load_model():
-    model = tensorflow.keras.models.load_model('best_model1.keras')
-    return model
-
-model = load_model()
+    return tensorflow.keras.models.load_model('best_model1.keras')
 
 # Function to preprocess the image
 def preprocess_image(img):
@@ -19,29 +20,46 @@ def preprocess_image(img):
     img_array = preprocess_input(img_array)  # Preprocess the image
     return img_array
 
+# Function to diagnose uploaded image
+def diagnose_image(uploaded_file, model):
+    # Display uploaded image and perform diagnosis
+    if uploaded_file is not None:
+        # Preprocess the image
+        processed_image = preprocess_image(image.load_img(uploaded_file, target_size=(224, 224)))
+
+        # Make predictions
+        prediction = model.predict(processed_image)
+        predicted_class = np.argmax(prediction)
+        
+        if predicted_class == 0:
+            result = "Adenocarcinoma"
+        elif predicted_class == 1:
+            result = "Large Cell Carcinoma"
+        elif predicted_class == 3: 
+            result = "Squamous Cell Carcinoma"
+        else:
+            result = "Normal"
+            # Display diagnosis result
+            st.write("Diagnosis:", ':green[' + result + ']')
+            return
+
+        # Display diagnosis result
+        st.write("Diagnosis", ':orange[' + result + ']')
+        return
+
 # Streamlit app
-st.title("Medical Image Diagnosis")
+st.title("NSCLC Diagnosis")
+
+# Load model
+model = load_model()
 
 # Upload image
 uploaded_file = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
 
-# Display uploaded image and perform diagnosis
+# Display uploaded image
 if uploaded_file is not None:
-    # Read the image file
-    img = image.load_img(uploaded_file, target_size=(224, 224))
-    st.image(img, caption="Uploaded Image", use_column_width=True)
-
-    # Preprocess the image
-    processed_image = preprocess_image(img)
-
-    # Make predictions
-    prediction = model.predict(processed_image)
-    predicted_class = np.argmax(prediction)
-    class_confidence = prediction[0][predicted_class]
-
-    # Get the class label
-    class_label = "Class " + str(predicted_class)
-
-    # Display diagnosis result
-    st.write("Diagnosis:", class_label)
-    st.write("Confidence:", class_confidence)
+    st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+  
+# Create a horizontal layout for buttons
+if st.button("Diagnose", help="Perform Diagnosis", use_container_width=True):
+    diagnose_image(uploaded_file, model)
